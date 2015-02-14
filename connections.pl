9a43a3f42bd31f0f65aa5cfae67005f834ec3806
Gene Boggs <gene.boggs@gmail.com>
2015-02-14 12:35:45 -0500

    Add handy comments. Remove debugging code

--- connections.pl
+++ connections.pl
@@ -6,43 +6,49 @@ use DBI;
 use Graph::Easy;
 use File::Temp qw( tempfile );
 
+# Connect to the database
 my $dsn  = 'dbi:mysql:database=connections;host=localhost;port=3306';
 my $user = 'root',
 my $pass = 'abc123';
 my $dbh  = DBI->connect( $dsn, $user, $pass, { RaiseError => 1, AutoCommit => 0 } );
 
+# Gather the machines by id
 my $sql = 'SELECT * FROM machine m';
 my $sth = $dbh->prepare($sql);
 my $rv  = $sth->execute();
 my $rs  = $sth->fetchall_hashref('id');
 $sth->finish;
-#use Data::Dumper::Concise;print Dumper($rs);
 
+# Make an id => name reference
 my $machine;
 $machine->{$_} = $rs->{$_}{name} for keys %$rs;
-#use Data::Dumper::Concise;warn Dumper$machine;
 
+# Gather the connected machines by id
 $sql = 'SELECT * FROM master_slave ms';
 $sth = $dbh->prepare($sql);
 $rv  = $sth->execute();
 $rs = $sth->fetchall_hashref('id');
 $sth->finish;
-#use Data::Dumper::Concise;print Dumper($rs);
 
+# Disconnect from the database
 $dbh->disconnect;
 
+# Create a new graph object
 my $g = Graph::Easy->new( { debug => 0, undirected => 1 } );
 
+# Assign edges to the graph by machine name
 for my $r ( keys %$rs )
 {
 #    print $machine->{ $rs->{$r}{master} } . ' <-> ' . $machine->{ $rs->{$r}{slave} }, "\n";
     $g->add_edge( $machine->{ $rs->{$r}{master} }, $machine->{ $rs->{$r}{slave} } );
 }
 
+# Output the graph in various formats
 #print $g->as_ascii;
 #print $g->as_graphml;
 #print $g->as_svg;
 
+# Use Graphviz to display the graph
 my ( $fh, $filename ) = tempfile();
 print $fh $g->as_graphviz;
 my @cmd = ( '/usr/local/bin/twopi', '-Tsvg', $filename );
