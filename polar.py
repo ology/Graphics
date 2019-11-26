074b325bf7330d0d1aa933d33b0b6ad5a5cf5d9e
Gene Boggs <gene@ology.net>
2019-11-25 23:24:37 -0800

    Moved from personal devel repo

---
+++ polar.py
@@ -0,0 +1,73 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#np.random.seed(42)
+
+# Set the boudaries
+n = 20
+high = 3
+
+# Get the data
+theta = np.linspace(0.0, 2 * np.pi, n, endpoint=False)
+radii = 10 * np.random.rand(n)
+
+# Make a polar scatter plot
+ax = plt.subplot(111, projection='polar')
+ax.scatter(theta, radii)
+
+# Make a dataframe
+df = pd.DataFrame({"theta": theta, "radii": radii})
+#samp = df.sample(n)
+#plt.plot(samp.theta, samp.radii, 'k-')
+
+# Make a random network of index nodes column
+net = []
+
+for i in range(len(df)):
+    roll = np.random.randint(low=0, high=high)
+    #print('Roll:', roll)
+
+    nodes = []
+    
+    for j in range(roll):
+        nodes.append(str(np.random.randint(low=0, high=n)))
+    
+    net.append(' '.join(nodes))
+
+df['net'] = net
+print(df)
+
+# Plot lines between the nodes based on the net column
+for i in range(len(df)):
+    for j in df.loc[i, 'net'].split():
+        #print(i, j)
+
+        x = []
+        y = []
+
+        x0 = df.loc[i, 'theta']
+        y0 = df.loc[i, 'radii']
+
+        x.append(x0)
+        y.append(y0)
+
+        j = int(j)
+
+        x1 = df.loc[j, 'theta']
+        y1 = df.loc[j, 'radii']
+
+        x.append(x1)
+        y.append(y1)
+
+        # Toggle 1=dashed lines, 0=green arrows
+        if 0:
+            plt.plot(x, y, 'b:')
+        else:
+            ax.annotate('',
+                xy=(x0, y0), xycoords='data',
+                xytext=(x1, y1), textcoords='data',
+                arrowprops=dict(arrowstyle='->', connectionstyle='arc3', color='green'),
+                )
+
+plt.show()
