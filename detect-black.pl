fc83e9c4dd9d6e37ccc685b318122ab0dc7fb9b1
Gene Boggs <gene@ology.net>
2020-03-07 22:26:18 -0800

    Add comment

--- detect-black.pl
+++ detect-black.pl
@@ -60,6 +60,7 @@ for my $key (sort { $prop{$a} <=> $prop{$b} } keys %prop) {
 }
 
 __END__
+# Commands used to create the graph
 $ perl detect-black.pl > /Users/gene/tmp/data.txt
 R> data <- read.csv('/Users/gene/tmp/data.txt', header=F)
 R> plot(data$V2, type='l', main='Ink Used For Each Letter', xlab='Sorted Letters', ylab='Ink Used', xaxt="n")
