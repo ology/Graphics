37f223c076fde86dca3fdd5516cd42ee0eccbdc7
Gene Boggs <gene@ology.net>
2020-03-07 22:23:16 -0800

    Reorder R commands

--- detect-black.pl
+++ detect-black.pl
@@ -62,6 +62,6 @@ for my $key (sort { $prop{$a} <=> $prop{$b} } keys %prop) {
 __END__
 $ perl detect-black.pl > /Users/gene/tmp/data.txt
 R> data <- read.csv('/Users/gene/tmp/data.txt', header=F)
-R> l <- seq(1, 26, 1)
 R> plot(data$V2, type='l', main='Ink Used For Each Letter', xlab='Sorted Letters', ylab='Ink Used', xaxt="n")
+R> l <- seq(1, 26, 1)
 R> for(i in l) axis(1, at=i, labels=data$V1[i], cex.axis = 1)
