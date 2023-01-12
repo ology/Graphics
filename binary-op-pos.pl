4887f09d5e36dab4ee0a97f8c0dfd2e0f6119337
Gene Boggs <gene@ology.net>
2023-01-12 07:12:09 -0800

    Use OEIS

--- binary-op-pos.pl
+++ binary-op-pos.pl
@@ -14,8 +14,8 @@ use OEIS qw(oeis);
 my $oeis = shift || 10; # https://en.wikipedia.org/wiki/List_of_integer_sequences
 my @oeis = oeis($oeis);
 
-#my $modulo = @oeis; # for OEIS processing
-my $modulo = 3;     # 3, 5, etc for simple modulo
+my $modulo = @oeis; # for OEIS processing
+#my $modulo = 3;     # 3, 5, etc for simple modulo
 
 my ($x, $y) = (1000, 1000);
 
@@ -37,7 +37,7 @@ for my $i (0 .. $x - 1) {
 #        if (($i ^ $j) % $modulo == 0) { # for simple modulo
 #        if (is_prime($i ^ $j)) {        # for prime processing
 #        if (isfibonacci($i ^ $j)) {     # for fibonacci processing
-#        if (grep { $_ == (($i ^ $j) % $modulo) } @oeis) {  # for OEIS processing
+        if (grep { $_ == (($i ^ $j) % $modulo) } @oeis) {  # for OEIS processing
             $img->setpixel(x => $i, y => $j, color => 'black');
         }
     }
