7e37e9ef845e7ed126bab709cae711491e4c23b3
Gene Boggs <gene@ology.net>
2023-01-12 07:10:38 -0800

    Fix comments

--- binary-op-pos.pl
+++ binary-op-pos.pl
@@ -34,9 +34,9 @@ $img->box(
 # inspect each image position
 for my $i (0 .. $x - 1) {
     for my $j (0 .. $y - 1) {
-        if (($i ^ $j) % $modulo == 0) {                  # for simple modulo
-#        if (is_prime($i ^ $j)) {             # for prime processing
-#        if (isfibonacci($i ^ $j)) {          # for fibonacci processing
+#        if (($i ^ $j) % $modulo == 0) { # for simple modulo
+#        if (is_prime($i ^ $j)) {        # for prime processing
+#        if (isfibonacci($i ^ $j)) {     # for fibonacci processing
 #        if (grep { $_ == (($i ^ $j) % $modulo) } @oeis) {  # for OEIS processing
             $img->setpixel(x => $i, y => $j, color => 'black');
         }
