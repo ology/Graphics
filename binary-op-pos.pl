47a65ac914086a5499b1dbac431783e45833a6b6
Gene Boggs <gene@ology.net>
2023-01-12 07:10:09 -0800

    Fix math

--- binary-op-pos.pl
+++ binary-op-pos.pl
@@ -14,8 +14,8 @@ use OEIS qw(oeis);
 my $oeis = shift || 10; # https://en.wikipedia.org/wiki/List_of_integer_sequences
 my @oeis = oeis($oeis);
 
-my $modulo = @oeis; # for OEIS processing
-#my $modulo = 3;     # 3, 5, etc for simple modulo
+#my $modulo = @oeis; # for OEIS processing
+my $modulo = 3;     # 3, 5, etc for simple modulo
 
 my ($x, $y) = (1000, 1000);
 
@@ -34,12 +34,10 @@ $img->box(
 # inspect each image position
 for my $i (0 .. $x - 1) {
     for my $j (0 .. $y - 1) {
-        my $bit = $i ^ $j;
-        my $mod = $x % $modulo;
-#        if ($mod == 0) {                  # for simple modulo
-#        if (is_prime($bit)) {             # for prime processing
-#        if (isfibonacci($bit)) {          # for fibonacci processing
-        if (grep { $_ == $mod } @oeis) {  # for OEIS processing
+        if (($i ^ $j) % $modulo == 0) {                  # for simple modulo
+#        if (is_prime($i ^ $j)) {             # for prime processing
+#        if (isfibonacci($i ^ $j)) {          # for fibonacci processing
+#        if (grep { $_ == (($i ^ $j) % $modulo) } @oeis) {  # for OEIS processing
             $img->setpixel(x => $i, y => $j, color => 'black');
         }
     }
